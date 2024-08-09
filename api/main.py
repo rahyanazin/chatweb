e1d49a17687d796309db78e0ec9f9cc11e2f3a93
rahyan-truxt <146733519+rahyan-truxt@users.noreply.github.com>
2024-08-09 18:49:43 -0300

    updates

--- api/main.py
+++ api/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+
+from typing import Optional
+
+import uuid
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get("/create-session")
+def create_session() -> str:
+
+    return uuid.uuid4().hex
+
+
+@app.post("/index")
+def index(url: str = Form(default="")):
+
+    return url
+
+
+@app.post('/ask')
+def ask(
+    url: str = Form(default=""),
+    query: str = Form(default=""),
+    session_id: Optional[str] = Form(default="")
+):
+    return f"answer for question '{query}' about url '{url}' in session '{session_id}'"
